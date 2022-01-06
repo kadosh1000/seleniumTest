9088e88ed05dc31cd8cf4d180d9f643a02540ff2
Matan Kadosh <kadosh10001@gmail.com>
2022-01-06 16:04:01 +0200

    use chromedriver pack binary

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const { Builder, By } = require("selenium-webdriver");
-var assert = require('assert');
+var assert = require("assert");
+
+// To prevenet the need to install chromedriver on the agent or doing any PATH handling
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
 async function checkIfTitleExistsAndNavigate() {
-const driver = await new Builder().forBrowser("chrome").build();
-await driver.get('https://phptravels.com/demo');
-const headerText = await (await driver.findElement(By.id('header-title'))).getAttribute("innerHTML");
-assert.equal('Application Test Drive', headerText);
-pressOnIntegrationsTab(driver);
+    const driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://phptravels.com/demo");
+    const headerText = await (await driver.findElement(By.id("header-title"))).getAttribute("innerHTML");
+    assert.equal("Application Test Drive", headerText);
+    pressOnIntegrationsTab(driver);
 }
-async function pressOnIntegrationsTab(driver){
-await (await driver.findElement(By.className("clearfix"))).click();
+async function pressOnIntegrationsTab(driver) {
+    await (await driver.findElement(By.className("clearfix"))).click();
 }
 
 (async function () {
-    return await checkIfTitleExistsAndNavigate();
-})().then().catch(err=>{console.error(err); process.exit(1);});
+    try{
+        await checkIfTitleExistsAndNavigate();
+        process.exit(1);
+    }
+    catch (err){
+        console.error(err);
+        process.exit(1);
+    }
+})()
